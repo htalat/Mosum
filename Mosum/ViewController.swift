479a9e3dbe25ac7ab3260b08b2fe884c33da02ed
hassan <hassan.talat.92@gmail.com>
2016-04-10 23:14:22 -0700

    Initial Commit

---
+++ Mosum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mosum
+//
+//  Created by Hassan Talat on 4/10/16.
+//  Copyright © 2016 Buff Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
